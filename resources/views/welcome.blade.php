53a8578d9acfd50c2ce241320a45ff17a69823cd
Davey Contreras <contreras.davey@gmail.com>
2017-12-13 01:52:03 -0500

    Basic templating.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,38 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+    <div class="container">
+        <div class="jumbotron jumbotron-md">
+            <h2>Simplify your links</h2>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+            @include ('url.create')
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+            <p>Track your tinyurl here: <span class="text-info">{{ url('/') }}/url/{<code>code</code>}</span> </p>
+        </div>
+        <div class="panel panel-default hidden" id="url-results">
+            <div class="panel-heading">
+                <h2>
+                    Here is your TinyUrl:
+                </h2>
+            </div>
 
-            .full-height {
-                height: 100vh;
-            }
+            <div class="panel-body">
+                <table class="table table-striped">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+                    <thead>
+                    <tr>
+                        <th>Original URL</th>
+                        <th>Created</th>
+                        <th>TinyUrl</th>
+                        <th>Views</th>
 
-            .position-ref {
-                position: relative;
-            }
+                    </tr>
+                    </thead>
+                    <tbody>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                    </tbody>
+                </table>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
